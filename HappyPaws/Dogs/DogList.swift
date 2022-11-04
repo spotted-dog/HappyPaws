4635f9e5dd18971d7d07a5fdfa23cbc31263507f
Hap Pearman <greg.pearman@spotteddogsoftware.net>
2022-11-03 20:19:22 -0600

    Add new dog

--- HappyPaws/Dogs/DogList.swift
+++ HappyPaws/Dogs/DogList.swift
@@ -9,37 +9,113 @@ import SwiftUI
 
 struct DogList: View {
   @Environment(\.managedObjectContext) private var viewContext
+  @Environment(\.dismiss) var dismiss
 
-  var dogs: [Dog]
+  @State var addDog = false
+  @State var addDogName = ""
+  @State var addDogFavoriteToy = ""
+  @State var addDogDateOfBirth = Date()
+
+  @FetchRequest(
+    sortDescriptors: [NSSortDescriptor(keyPath: \Canine.name, ascending: true)],
+    animation: .default)
+  private var canines: FetchedResults<Canine>
+
+  @State var dogs: [Dog]
+
+  var limitRange: ClosedRange<Date> {
+    let twentyYearsAgo = Calendar.current.date(byAdding: .year, value: -20, to: Date())!
+    let today = Date()
+
+    return twentyYearsAgo...today
+  }
 
   var body: some View {
     let dogsSorted = dogs.sorted(by: <)
-    List {
-      ForEach(dogsSorted) { dog in
-        NavigationLink(destination: DogDetailView(dog: dog)) {
-          DogListRow(dog: dog)
+
+    NavigationView {
+      List {
+        ForEach(dogsSorted) { dog in
+          NavigationLink(destination: DogDetailView(dog: dog)) {
+            DogListRow(dog: dog)
+          }
         }
       }
-      .onDelete(perform: deleteDogs)
-    }
-    .navigationBarTitle("All Dogs")
-    .toolbar {
-      ToolbarItem(placement: .navigationBarTrailing) {
-        EditButton()
+      .navigationBarTitle("All Dogs")
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Button(action: {
+            self.addDog.toggle()
+          }, label: {
+            Image(systemName: "plus")
+          })
+        }
       }
-      ToolbarItem {
-        Button(action: addDog) {
-          Label("Add Dog", systemImage: "plus")
+    }
+    .sheet(isPresented: $addDog) {
+      VStack {
+        HStack {
+          Text("Name: ")
+            .font(.system(.title))
+            .fontWeight(.bold)
+            .foregroundColor(.gray)
+          TextField("Dogs Name", text: self.$addDogName)
+            .font(.system(.title))
+            .foregroundColor(.gray)
+        }
+
+        DatePicker("Date of Birth",
+                   selection: self.$addDogDateOfBirth,
+                   in: limitRange,
+                   displayedComponents: [.date])
+          .padding()
+
+        HStack {
+          Text("Favorite Toy: ")
+            .font(.system(.title))
+            .fontWeight(.bold)
+            .foregroundColor(.gray)
+          TextField("Dogs favorite toy", text: self.$addDogFavoriteToy)
+            .font(.system(.title))
+            .foregroundColor(.gray)
+        }
+
+        HStack {
+          Button(action: {
+            self.addDogAction()
+            self.addDog.toggle()
+            dismiss()
+          }, label: {
+            Text("Add")
+              .font(.system(.title))
+              .fontWeight(.bold)
+          })
+          .padding()
+
+          Button(action: {
+            self.addDog.toggle()
+            dismiss()
+          }, label: {
+            Text("Cancel")
+              .font(.system(.title))
+              .fontWeight(.bold)
+          })
+          .padding()
         }
       }
     }
   }
 
-  private func addDog() {
+  private func addDogAction() {
     withAnimation {
-      let newDog = Canine(context: viewContext)
-      newDog.id = UUID()
-      newDog.name = "Haps New Dog"
+      let newDog = Dog(name: self.addDogName,
+                       dateOfBirth: self.addDogDateOfBirth,
+                       image: nil,
+                       favoriteToy: self.addDogFavoriteToy,
+                       isFavorite: false)
+      dogs.append(newDog)
+
+      _ = newDog.createCanine(context: viewContext)
 
       do {
         try viewContext.save()
@@ -53,31 +129,6 @@ struct DogList: View {
       }
     }
   }
-
-  private func deleteDogs(offsets: IndexSet) {
-//    var canines = [Canine]()
-//    
-//    offsets.map { dogs[$0] }.forEach {
-//      let canine = dogs[$0].createCanine(from: dogs[$0], context: viewContext)
-//      canines.append(canine)
-//    }
-//    
-//    withAnimation {
-//      offsets.map { dogs[$0] }.forEach(viewContext.delete)
-//      
-//      do {
-//        try viewContext.save()
-//      } catch {
-//        // Replace this implementation with code to handle the error appropriately.
-          // fatalError() causes the application to generate a crash log and terminate.
-          // You should not use this function in a shipping application, although it
-          // may be useful during development.
-//        let nsError = error as NSError
-//        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//      }
-//    }
-  }
-
 }
 
 struct DogList_Previews: PreviewProvider {
--- HappyPaws/Dogs/DogsView.swift
+++ HappyPaws/Dogs/DogsView.swift
@@ -19,11 +19,6 @@ struct DogsView: View {
     didSet { isDisplayingError = true }
   }
 
-  @FetchRequest(
-    sortDescriptors: [NSSortDescriptor(keyPath: \Canine.name, ascending: true)],
-    animation: .default)
-  private var dogs: FetchedResults<Canine>
-
   @State private var selectedTab = 1
 
   var body: some View {
@@ -37,17 +32,13 @@ struct DogsView: View {
           }
           .tag(0)
 
-        NavigationView {
-          DogList(dogs: petStore.dogs)
-          Text("Select a Dog")
-        }
-        .tabItem {
-          Image(systemName: "house.fill")
-          Text("Dog House")
-            .accessibilityIdentifier("DogHouseTab")
-
-        }
-        .tag(1)
+        DogList(dogs: petStore.dogs)
+          .tabItem {
+            Image(systemName: "house.fill")
+            Text("Dog House")
+              .accessibilityIdentifier("DogHouseTab")
+          }
+          .tag(1)
 
         BreedsView(breeds: petStore.dogBreeds)
           .tabItem {
@@ -75,42 +66,6 @@ struct DogsView: View {
             lastErrorMessage = error.localizedDescription
           }
         }
-    }
-  }
-
-  private func addDog() {
-    withAnimation {
-      let newDog = Canine(context: viewContext)
-      newDog.id = UUID()
-      newDog.name = "Haps New Dog"
-
-      do {
-        try viewContext.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate.
-        // You should not use this function in a shipping application, although it
-        // may be useful during development.
-        let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-      }
-    }
-  }
-
-  private func deleteDogs(offsets: IndexSet) {
-    withAnimation {
-      offsets.map { dogs[$0] }.forEach(viewContext.delete)
-
-      do {
-        try viewContext.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate.
-        // You should not use this function in a shipping application, although it
-        // may be useful during development.
-        let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-      }
     }
   }
 }
